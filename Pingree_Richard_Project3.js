fbd507fb604890a91bcd520c957458affcdd8c64
richpingree <richpingree@fullsail.edu>
2013-09-18 07:39:56 -0500

    added global variable, confirm, and procedure

--- Pingree_Richard_Project3.js
+++ Pingree_Richard_Project3.js
@@ -1,1 +1,35 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Richard Pingree
+// SDI 1309
+// Project 3
+
+
+//Variables
+
+var partyType = "pizza party",
+	numberOfPizzas = 4
+	soda = true
+	
+;	
+
+var ordering = confirm("Do you want to order pizza? Ok for yes or Cancel for no.")
+
+//functions
+
+var pizza = function (order) {
+	
+	if (order === true) {
+		
+		console.log("We are going to order " + numberOfPizzas + " pizzas.");
+		
+	} else {
+		
+		console.log("We are not hungry.");
+	}
+};
+
+
+//outputs
+
+pizza(ordering)
